d4b5d4b88cb497a1dc4de35c35db61b18cae4cde
kktom <2152144273@qq.com>
2025-11-22 20:35:37 +0800

    create list

--- app/(dashboard)/_components/Sidebar/NewButton.tsx
+++ app/(dashboard)/_components/Sidebar/NewButton.tsx
@@ -9,12 +9,12 @@ export const NewButton = () => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-aquare">
-          <button className="bg-white/25 h-full w-full rounded-lg flex items-center justify-center opacity-60 hover:opacity-100 transition p-1">
+          <button className="bg-white/25 h-full w-full rounded-lg flex items-center justify-center opacity-60 hover:opacity-100 transition p-2">
             <Plus className="text-white"></Plus>
           </button>
         </div>
       </DialogTrigger>
-      <DialogContent className="p-0  bg-transparent  border-none shadow-none w-1/2">
+      <DialogContent className="p-0  bg-transparent  border-none shadow-none min-w-[480px]">
         <CreateOrganization />
       </DialogContent>
     </Dialog>
