b2e36698e7baa45a5550165991c29325a018d363
kktom <2152144273@qq.com>
2025-11-22 20:21:17 +0800

    create orgnization dialog

---
+++ app/(dashboard)/_components/Sidebar/NewButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const NewButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-aquare">
+          <button className="bg-white/25 h-full w-full rounded-lg flex items-center justify-center opacity-60 hover:opacity-100 transition p-1">
+            <Plus className="text-white"></Plus>
+          </button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="p-0  bg-transparent  border-none shadow-none w-1/2">
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  );
+};
