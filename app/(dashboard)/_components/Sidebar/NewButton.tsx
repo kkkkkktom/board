5602d64df14aca6f03c6f387cc17c7b3322fb859
kktom <2152144273@qq.com>
2025-11-22 21:30:20 +0800

    create tooltop of item&newButton

--- app/(dashboard)/_components/Sidebar/NewButton.tsx
+++ app/(dashboard)/_components/Sidebar/NewButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Plus } from "lucide-react";
 import { CreateOrganization } from "@clerk/nextjs";
+import { Hint } from "@/components/hint";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
@@ -9,9 +10,16 @@ export const NewButton = () => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-aquare">
-          <button className="bg-white/25 h-full w-full rounded-lg flex items-center justify-center opacity-60 hover:opacity-100 transition p-2">
-            <Plus className="text-white"></Plus>
-          </button>
+          <Hint
+            label="Create organization"
+            side="right"
+            // align="start"
+            sideOffset={15}
+          >
+            <button className="bg-white/25 h-full w-full rounded-lg flex items-center justify-center opacity-60 hover:opacity-100 transition p-2">
+              <Plus className="text-white"></Plus>
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0  bg-transparent  border-none shadow-none min-w-[480px]">
