df45bbbe631480d10e37942797037ffbf6169c66
kktom <2152144273@qq.com>
2025-11-22 21:06:23 +0800

    create org list item

--- app/(dashboard)/_components/Sidebar/list.tsx
+++ app/(dashboard)/_components/Sidebar/list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useOrganizationList } from "@clerk/nextjs";
+import { Item } from "./item";
 export const List = () => {
   const { userMemberships } = useOrganizationList({
     userMemberships: {
@@ -11,7 +12,14 @@ export const List = () => {
   return (
     <ul className="space-y-4">
       {userMemberships.data?.map((mem) => {
-        return <p key={mem.organization.id}>{mem.organization.name}</p>;
+        return (
+          <Item
+            key={mem.organization.id}
+            id={mem.organization.id}
+            name={mem.organization.name}
+            imageUrl={mem.organization.imageUrl}
+          />
+        );
       })}
     </ul>
   );
